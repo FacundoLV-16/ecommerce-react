3ffa16dab5e57856dcfa4a9e5fcf1191f5786e2f
Facundo Matias Caino <facundocaino@gmail.com>
2022-09-01 19:34:33 -0300

    pract 1

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <body>
+      {/* BR */}
+      {/* BR */}
+      {/* BR */}
+      {/*           HEADER */}
+      <header>
+        <NavBar></NavBar>
+      </header>
+      {/*           HEADER */}
+      {/* BR */}
+      {/* BR */}
+      {/* BR */}
+      {/*           MAIN */}
+      <main></main>
+      {/*           MAIN */}
+      {/* BR */}
+      {/* BR */}
+      {/* BR */}
+      {/*           FOOTER */}
+      <footer></footer>
+      {/*           FOOTER */}
+      {/* BR */}
+      {/* BR */}
+      {/* BR */}
+      {/*           SCRIPT BOOSTRAP */}
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+      {/*           SCRIPT BOOSTRAP */}
+    </body>
+  );
+}
+
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,87 @@
 const NavBar = () => {
   return (
     <header>
-      <div>
-        <a href="#">
-          <img src="" alt="Logo" />
-        </a>
+      <div class="conteiner-all">
+        <div class="menu-superior">
+          {/* LOGO */}
+          <div class="logo-div">
+            <a href="index.html">
+              <img
+                class="img-movil"
+                src="https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.19.5/mercadolibre/logo__small.png"
+                alt="Mercado libre"
+              />
+              <img
+                class="img-pc"
+                src="https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.19.5/mercadolibre/logo__large_plus.png"
+                alt="Mercado libre"
+              />
+            </a>
+          </div>
+          {/* BUSCADOR */}
+          <form action="">
+            <input
+              type="search"
+              placeholder="Busca productos, marcas y más ..."
+            />
+          </form>
+          {/* LOGO */}
+          <img
+            class="img-propaganda"
+            src="https://http2.mlstatic.com/D_NQ_887100-MLA50801817839_072022-OO.webp"
+            alt="Propaganda"
+          />
+          <a href="" class="mi-cuenta">
+            <i class="fa-brands fa-empire"></i> Mi cuenta
+          </a>
+          <a href="" class="mis-compras">
+            Mis compras <i class="fa-solid fa-cart-shopping"></i>
+          </a>
+        </div>
+        {/* MENU */}
+        <did class="menu-inferior">
+          <div class="div-menu">
+            <ul class="menu">
+              <li></li>
+              <li></li>
+              <li>Categorias</li>
+              <li>
+                <a href="paginas/ofertas.html">Ofertas</a>
+              </li>
+              <li>Historias</li>
+              <li>Supermercado</li>
+              <li>Moda</li>
+              <li>Vender</li>
+              <li>Ayuda</li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li>
+                <a href="paginas/register.html">Crea tu cuenta</a>
+              </li>
+              <li>
+                <a href="paginas/user-session.html">Ingresá</a>
+              </li>
+              <li>
+                <a href="paginas/carro.html">Mis compras</a>
+              </li>
+            </ul>
+          </div>
+        </did>
       </div>
-      <nav>
-        <ul>
-          <li>Inicio</li>
-          <li></li>
-          <li>Ofertas</li>
-          <li></li>
-          <li>Login</li>
-          <li>Register</li>
-        </ul>
-      </nav>
+      {/* ALERTA */}
+      <div
+        class="alert container position-sticky top-0 alert-primary hide"
+        role="alert"
+      >
+        Producto Añadido al carrito!
+      </div>
+      <div
+        class="alert container position-sticky top-0 alert-danger remove"
+        role="alert"
+      >
+        Producto Removido!
+      </div>
     </header>
   );
 };
