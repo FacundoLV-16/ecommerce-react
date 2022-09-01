c039df7a9e3834b3fe7d344d3c005e130d1ee928
Facundo Matias Caino <facundocaino@gmail.com>
2022-09-01 11:54:41 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar></NavBar>
-    </div>
+    <body>
+      <header>
+        <NavBar></NavBar>
+      </header>
+      <main></main>
+      <footer></footer>
+    </body>
   );
 }
 
